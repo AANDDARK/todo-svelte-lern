e90c67421999b8c66ad0fc0992c1cd259fcfd66e
AANDDARK <andriyworkkovalenko@gmail.com>
2025-05-26 22:18:09 +0300

    work with key

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import Input from "./lib/input.svelte";
+    import Test from "./lib/test.svelte";
     import Todo from "./lib/todo.svelte";
   import { todos } from "./todos.svelte";
 
@@ -11,5 +12,8 @@
   {#each $todos as todo}
       <Todo name={todo.name}, text={todo.text} id={todo.id}/>
   {/each}
+
+  <hr />
+  <Test />
 </main>
 
---
+++ src/lib/test.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { fade } from "svelte/transition";
+
+    let value = $state(0)
+    $effect(() => {
+        setInterval(() => {
+            value += 1
+        }, 1000)    
+    })
+</script>
+
+{#key value}
+	<div transition:fade>{value}</div>
+{/key}
